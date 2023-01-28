0357fd6aa37edf47d73052db2413c86a3b1f108b
hendisantika <hendisantika@yahoo.co.id>
2023-01-28 20:39:52 +0700

    #1 - Initial Commit ✏️ 📗 🐳⬆

---
+++ src/main/java/com/hendisantika/springbootjedis/SpringBootJedisApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootjedis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJedisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootjedis/SpringBootJedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootjedis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
