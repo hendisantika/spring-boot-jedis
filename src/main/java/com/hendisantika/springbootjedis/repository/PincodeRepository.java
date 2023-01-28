dad055130bca15fe7fbbbef3830d22e50da58f1e
hendisantika <hendisantika@yahoo.co.id>
2023-01-28 20:45:05 +0700

    #4 - Add save in Pincode repository ✏️ 📗 🐳:octocat:

---
+++ src/main/java/com/hendisantika/springbootjedis/repository/PincodeRepository.java
@@ -0,0 +1,18 @@
+package com.hendisantika.springbootjedis.repository;
+
+import com.hendisantika.springbootjedis.model.Pincode;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jedis
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/28/23
+ * Time: 20:44
+ * To change this template use File | Settings | File Templates.
+ */
+public interface PincodeRepository {
+    void save(Pincode pincode);
+
+}
