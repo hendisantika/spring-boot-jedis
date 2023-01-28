e5cea297132b32edc22a143d11c8556531f40c26
hendisantika <hendisantika@yahoo.co.id>
2023-01-28 20:44:14 +0700

    #3 - Add Pincode Model ✏️ 📗 🐳:octocat:

---
+++ src/main/java/com/hendisantika/springbootjedis/model/Pincode.java
@@ -0,0 +1,14 @@
+package com.hendisantika.springbootjedis.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jedis
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/28/23
+ * Time: 20:41
+ * To change this template use File | Settings | File Templates.
+ */
+public record Pincode(long id, String pincodeVal, String stateName) {
+}
