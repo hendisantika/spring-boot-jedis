f9ffaa9c4b757ab56e585ffc87eb8af05dfa6add
hendisantika <hendisantika@yahoo.co.id>
2023-01-28 20:49:26 +0700

    #16 - Add redisTemplate Bean in RedisConfig ✏️ 📗 🐳:octocat:

--- src/main/java/com/hendisantika/springbootjedis/config/RedisConfig.java
+++ src/main/java/com/hendisantika/springbootjedis/config/RedisConfig.java
@@ -3,6 +3,8 @@ package com.hendisantika.springbootjedis.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,5 +21,13 @@ public class RedisConfig {
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
         return new JedisConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        final RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
+        return template;
     }
 }
